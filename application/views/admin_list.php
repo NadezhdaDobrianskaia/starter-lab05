368a5c9bdde4ef0c238d56f97095ec5a64ac643a
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-10-22 15:18:06 -0700

    adding a maintenance controller

---
+++ application/views/admin_list.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table cols="" border ="0">
+            <tr>
+                <th>ID</th>
+                <th>Author</th>
+                <th>Mugshot</th>
+                <th>Their Saying</th>
+            </tr>
+            {quotes}
+            <tr>
+                <th>{id}</th>
+                <th>{who}</th>
+                <th>{mug}</th>
+                <th>{what}</th>
+            </tr>
+            {/quotes}
+        </table>
+    </body>
+</html>
